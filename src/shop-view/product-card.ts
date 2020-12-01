73be6199d3c14a2a64532256f2ea2087b5f8680c
Kyle Andrews <codingwithkyle@gmail.com>
2020-12-01 13:42:56 -0500

    Added description view to product cards

--- src/shop-view/product-card.ts
+++ src/shop-view/product-card.ts
@@ -1,14 +1,40 @@
-export class ProductCard extends HTMLElement{
+import { Component } from "../component";
+
+type ProductCardState = {
+    view: "primary" | "secondary";
+}
+export class ProductCard extends Component<ProductCardState>{
     private addToCartButton:HTMLButtonElement;
+    private toggleDescriptionButton:HTMLButtonElement;
 
     constructor(){
         super();
-        this.addToCartButton = this.querySelector("button");
+        this.state = {
+            view: "primary",
+        };
+        this.addToCartButton = this.querySelector(".js-add-to-cart-button");
+        this.toggleDescriptionButton = this.querySelector(".js-description-button");
     }
 
     private addToCart:EventListener = ()=>{}
 
-    connectedCallback(){
+    private toggleDescription:EventListener = ()=>{
+        this.setState({view: this.state.view === "primary" ? "secondary" : "primary"});
+    }
+
+    connected(){
         this.addToCartButton.addEventListener("click", this.addToCart);
+        this.toggleDescriptionButton.addEventListener("click", this.toggleDescription);
+    }
+
+    render(){
+        this.setAttribute("view", this.state.view);
+        if (this.state.view === "primary"){
+            this.toggleDescriptionButton.setAttribute("title", "view description");
+            this.toggleDescriptionButton.setAttribute("aria-label", "view description");
+        }else{
+            this.toggleDescriptionButton.setAttribute("title", "close description");
+            this.toggleDescriptionButton.setAttribute("aria-label", "close description");
+        }
     }
 }
