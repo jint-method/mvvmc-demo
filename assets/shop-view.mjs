534e9180af95fe115eac98caf3a1a751fa22835b
jint-method <codingwithkyle@gmail.com>
2020-12-03 19:17:24 +0000

    Updates

--- assets/shop-view.mjs
+++ assets/shop-view.mjs
@@ -56,7 +56,7 @@ export default class ShopView extends Component {
                                     <product-card class="bg-white border-1 border-solid border-grey-300 radius-0.5" view="primary">
                                         <card-content class="block w-full h-full">
                                             <img-shim>
-                                                <img draggable="false" src="/images/${product.filename}" alt="${product.description}" />
+                                                <img draggable="false" src="/images/${product.image}" alt="${product.alt}" />
                                             </img-shim>
                                             <h2 class="w-full line-snug text-capitalize font-medium" flex="justify-between row nowrap">
                                                 <span style="flex: 1;" class="inline-block font-grey-800">${product.title}</span>
