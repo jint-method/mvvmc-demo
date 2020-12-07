c51a9602b30177a0417756c04930f08c25d5c000
jint-method <codingwithkyle@gmail.com>
2020-12-07 16:39:16 +0000

    Updates

--- assets/cart.mjs
+++ assets/cart.mjs
@@ -11,6 +11,7 @@ class Cart {
             const lineItem = { ...product };
             lineItem.qty = 1;
             this.lineItems[productUid] = lineItem;
+            // TODO: send new line item to the cart on the server
             message({
                 recipient: "cart",
                 type: "add-line-item",
@@ -20,19 +21,12 @@ class Cart {
             });
         }
         else {
-            this.lineItems[productUid].qty++;
-            message({
-                recipient: "cart",
-                type: "update-line-item",
-                data: {
-                    uid: productUid,
-                    qty: this.lineItems[productUid].qty,
-                },
-            });
+            this.updateLineItem(productUid, this.lineItems[productUid].qty + 1);
         }
     }
     updateLineItem(productUid, qty) {
         this.lineItems[productUid].qty = qty;
+        // TODO: send new quantity to the cart on the server
         message({
             recipient: "cart",
             type: "update-line-item",
@@ -44,6 +38,7 @@ class Cart {
     }
     removeLineItem(productUid) {
         delete this.lineItems[productUid];
+        // TODO: remove line item from the cart on the server
         message({
             recipient: "cart",
             type: "remove-line-item",
